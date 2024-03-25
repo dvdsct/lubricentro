852ef0de79bbc9f19e1da1c94e06b4c2ca82e222
Vale <valen_m95@hotmail.com>
2024-03-25 17:07:28 -0300

    [ADD]Pedidos Proveedores, componente livewire +[EDIT]Colores Hexadecimal

--- database/migrations/2024_03_18_215702_create_colores_table.php
+++ database/migrations/2024_03_18_215702_create_colores_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('colores', function (Blueprint $table) {
             $table->id();
             $table->string('descripcion');
+            $table->string('hexadecimal');
             $table->string('estado');
             $table->timestamps();
         });
--- database/seeders/ColoresSeeder.php
+++ database/seeders/ColoresSeeder.php
@@ -14,37 +14,39 @@ class ColoresSeeder extends Seeder
     public function run(): void
     {
 
-        $colores = ['Blanco',
-        'Negro',
-        'Plateado',
-        'Gris',
-        'Azul',
-        'Rojo',
-        'Amarillo',
-        'Verde',
-        'Marrón',
-        'Naranja',
-        'Beige',
-        'Dorado',
-        'Blanco perla',
-        'Rojo cereza',
-        'Gris oscuro',
-        'Azul marino',
-        'Verde oliva',
-        'Rosa',
-        'Morado',
-        'Cyan',
-        'Magenta',
-        'Turquesa',
-        'Bronce',
-        'Platino'
+        $colores = [
+        ['Blanco', '#ffffff'],
+        ['Negro', '#000000'],
+        ['Plateado', '#c0c0c0'],
+        ['Gris', '#808080'],
+        ['Azul', '#0000ff'],
+        ['Rojo', '#ff0000'],
+        ['Amarillo', '#ffff00'],
+        ['Verde', '#008000'],
+        ['Marrón', '#964b00'],
+        ['Naranja', '#ff7f00'],
+        ['Beige', '#f5f5dc'],
+        ['Dorado', '#ffd700'],
+        ['Blanco perla', '#eae0c8'],
+        ['Rojo cereza', '#da1884'],
+        ['Gris oscuro', '#a9a9a9'],
+        ['Azul marino', '#000080'],
+        ['Verde oliva', '#808000'],
+        ['Rosa', '#ff007f'],
+        ['Morado', '#800080'],
+        ['Cyan', '#00ffff'],
+        ['Magenta', '#ff00ff'],
+        ['Turquesa', '#40e0d0'],
+        ['Bronce', '#cd7f32'],
+        ['Platino', '#e5e4e2'],
         ];
 
 
         foreach ($colores as $color){
 
             Colores::create([
-                'descripcion'=>$color,
+                'descripcion'=>$color[0],
+                'hexadecimal'=>$color[1],
                 'estado'=>'1'
             ]);
         }
