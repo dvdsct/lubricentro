8b54d4dbe15583a6b2423b7a3cae743d34bd1729
Nahuel <nahuelmartinez07@gmail.com>
2024-08-15 22:00:54 -0300

    [EDIT] PDF_Stock

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -9,10 +9,14 @@ class Stock extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['cantidad','estado','sucursal_id','producto_id'];
+    protected $fillable = ['cantidad','estado','sucursal_id','producto_id','stock_id'];
 
     public function productos(){
 
         return $this->belongsTo(Producto::class,'producto_id');
+    }
+
+    public function stocks(){
+        return $this->belongsTo(Stock::class,'stock_id');
     }
 }
--- resources/views/pdf/stock.blade.php
+++ resources/views/pdf/stock.blade.php
@@ -6,18 +6,86 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
-    <title>Orden de Trabajo</title>
-
+    <title>Stock - Rocket</title>
 
 </head>
 
+<style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        @media print {
+            body {
+                margin: 0;
+                padding: 0;
+            }
+
+            table {
+                width: 100%;
+                border: 1px solid black;
+            }
+
+            th, td {
+                border: 1px solid black;
+                padding: 10px;
+            }
+
+            th {
+                background-color: #d9d9d9;
+            }
+        }
+    </style>
 
 <body>
 
 
-    @foreach ($stockActual as $item)
-        {{ $item->cantidad }} <br>{{$item->productos->precio}}
-    @endforeach
+    
+    <h1>Stock Actual de Productos</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Codigo Rocket</th>
+                <th>Producto</th>
+                <th>Cantidad</th>
+                <th>Precio Vta</th>
+                <th>Stock</th>
+            </tr>
+        </thead>
+        <tbody>
+        @foreach ($stockActual as $item)
+            <tr>
+                <td>{{$item->productos->id}}</td>
+                <td>{{$item->productos->descripcion}} {{$item->productos->codigo}}</td>
+                <td>{{ $item->cantidad }}</td>
+                <td>${{$item->productos->precio_venta}}</td>
+                <td>  {{ $item->productos->stocks->sum('cantidad') }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+  
 </body>
 
 </html>
